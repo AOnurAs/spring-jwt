e12dd26b1dc82b524331bc18f8e1a25e73516566
Ali Onur Aslan <onuras0399@hotmail.com>
2025-07-21 10:55:36 +0300

    - Implemented SecurityConfig with JWT-based security filters. - Created JWTAuthenticationFilter for token validation on incoming requests. - Added ApplicationConfig to wire necessary beans (e.g., AuthenticationManager). - JwtService and supporting classes are now functional and ready to be used by future service-layer implementations.

---
+++ spring-jwt/src/main/java/com/AOA/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.AOA.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.AOA.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	//@Query(value = "from User where username = :username")
+	Optional<User> findByUsername(String username);
+}
